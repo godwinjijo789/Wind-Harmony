c2136ee240ed87c664767457ca79d9e5bf6569cc
godwinjijo789 <311824201010@msajce-edu.in>
2025-08-13 06:09:00 +0530

    Update Otp.js

--- client/Otp.js
+++ client/Otp.js
@@ -1,14 +1,14 @@
 <script src="https://cdn.jsdelivr.net/npm/emailjs-com@3/dist/email.min.js"></script>
 <script>
 (function(){
-   emailjs.init("YOUR_PUBLIC_KEY"); // EmailJS-ல் இருந்து கிடைக்கும் Public Key
+   emailjs.init("m01hblbyvptOezpAv"); // EmailJS-ல் இருந்து கிடைக்கும் Public Key
 })();
 
 function sendOTP() {
     let otp = Math.floor(100000 + Math.random() * 900000); // 6 digit OTP
     localStorage.setItem("otp", otp);
 
-    emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", {
+    emailjs.send("service_j9lzcze", "template_6rspekz", {
         otp_code: otp,
         to_email: document.getElementById("email").value
     }).then(function() {
